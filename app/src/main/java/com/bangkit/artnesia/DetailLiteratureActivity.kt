b2373a11dc4ee3cff516bf276fb9924246931b68
Ryan Teguh <ryanteguh123@gmail.com>
2022-05-16 02:37:04 +0800

    Add Explore Fragment and Literature Detail UI

---
+++ app/src/main/java/com/bangkit/artnesia/DetailLiteratureActivity.kt
@@ -0,0 +1,11 @@
+package com.bangkit.artnesia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailLiteratureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_literature)
+    }
+}
